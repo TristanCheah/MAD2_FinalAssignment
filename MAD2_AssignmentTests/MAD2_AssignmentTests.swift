816ee0f6804dc72d231e8ff05ce86cfc8f01ce15
Tristan Cheah, Jedidiah Wong <cheahtristan@hotmail.sg>
2020-01-16 15:40:04 +0800

    First commit

---
+++ MAD2_AssignmentTests/MAD2_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  MAD2_AssignmentTests.swift
+//  MAD2_AssignmentTests
+//
+//  Created by Tristan Cheah on 16/1/20.
+//  Copyright © 2020 Tristan Cheah. All rights reserved.
+//
+
+import XCTest
+@testable import MAD2_Assignment
+
+class MAD2_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
