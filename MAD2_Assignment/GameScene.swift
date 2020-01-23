60d0b20ebe18af22513b4cf9c6741223e4d40036
Tristan Cheah, Jedidiah Wong <cheahtristan@hotmail.sg>
2020-01-23 18:56:19 +0800

    Joystick added, will now try to incorporate side scroller movement

--- MAD2_Assignment/GameScene.swift
+++ MAD2_Assignment/GameScene.swift
@@ -16,11 +16,30 @@ class GameScene: SKScene {
     
     private var lastUpdateTime : TimeInterval = 0
     
+    let player = SKSpriteNode(imageNamed: "player")
+    let ball = SKSpriteNode(imageNamed: "joystick_ball")
+    let base = SKSpriteNode(imageNamed: "joystick_base")
+    
+    var stickActive : Bool = false
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+        player.position = CGPoint(x: 100, y: 100);
+        player.scale(to: CGSize(width: 100, height: 100));
+        self.addChild(player)
         
+        self.addChild(base)
+        base.position = CGPoint(x: 0, y: -200)
+        base.scale(to: CGSize(width: 150, height: 150))
+        
+        self.addChild(ball)
+        ball.position = base.position
+        ball.scale(to: CGSize(width: 150, height: 150))
+        
+        base.alpha = 0.4
+        ball.alpha = 0.4
         
     }
     
@@ -40,15 +59,55 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
        
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches {
+            
+            let location = t.location(in: self)
+            if(base.frame.contains(location)){
+                stickActive = true
+            }
+            else{
+                stickActive = false
+            }
+            
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in (touches as Set<UITouch>){
+            let location = t.location(in: self)
+            if(stickActive == true){
+                let v = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
+                
+                let angle = atan2(v.dy, v.dx)
+                //let deg = angle * CGFloat(180/Double.pi)
+                
+                let length:CGFloat = base.frame.size.height/2
+                
+                let x_dist:CGFloat = sin(angle - 1.57079633) * length
+                let y_dist :CGFloat = cos(angle - 1.57079633) * length //90 ded in radians
+                
+                
+                
+                if(base.frame.contains(location)){
+                    ball.position = location
+                }
+                else{
+                    ball.position = CGPoint(x:base.position.x - x_dist, y:base.position.y + y_dist)
+                }
+                
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+            if (stickActive == true){
+                let move:SKAction = SKAction.move(to: base.position, duration: 0.2)
+                move.timingMode = .easeOut
+                
+                ball.run(move)
+            
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
